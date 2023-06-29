da72465829b8a60b42f07cff70bf13984bf75ffa
RTMecha <mechaprower2022@gmail.com>
2023-06-29 15:24:02 +1000

    Renamed ObjectModifersEditorHandler

---
+++ Functions/ObjectModifiersEditor.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace EditorManagement.Functions
+{
+    public class ObjectModifiersEditor : MonoBehaviour
+    {
+        public static ObjectModifiersEditor inst;
+
+        public static Type objectModifiersPlugin;
+
+        public static bool installed = false;
+
+        private void Awake()
+        {
+            if (!GameObject.Find("BepInEx_Manager").GetComponentByName("ObjectModifiersPlugin"))
+            {
+                Destroy(gameObject);
+            }
+            else
+            {
+                objectModifiersPlugin = GameObject.Find("BepInEx_Manager").GetComponentByName("ObjectModifiersPlugin").GetType();
+            }
+
+            inst = this;
+        }
+
+        public static void CreateHomingTimeline()
+        {
+
+        }
+
+        public static void CreateHomingDialog()
+        {
+
+        }
+
+        public static GameObject Keyframe()
+        {
+            return null;
+        }
+    }
+}
