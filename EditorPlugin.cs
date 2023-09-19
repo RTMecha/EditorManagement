b4689c14041828d1fb21b689f664b1791a7dd587
RTMecha <mechaprower2022@gmail.com>
2023-09-19 13:55:50 +1000

    Version: 1.10.3

--- EditorPlugin.cs
+++ EditorPlugin.cs
@@ -30,7 +30,7 @@ using RTFunctions.Functions.Managers;
 
 namespace EditorManagement
 {
-    [BepInPlugin("com.mecha.editormanagement", "Editor Management", " 1.10.2")]
+    [BepInPlugin("com.mecha.editormanagement", "Editor Management", " 1.10.3")]
 	[BepInProcess("Project Arrhythmia.exe")]
 	//[BepInIncompatibility("com.mecha.renderdepthunlimited")]
 	//[BepInIncompatibility("com.mecha.originoffset")]
