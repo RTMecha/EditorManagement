b33d0536c68fa32e58302e9ca76d1a0fb9dbb401
RTMecha <mechaprower2022@gmail.com>
2023-12-11 19:38:19 +1100

    Added AddEditorDropdown for simple adding of file dropdown buttons

--- Functions/Helpers/EditorHelper.cs
+++ Functions/Helpers/EditorHelper.cs
@@ -4,6 +4,10 @@ using System.Linq;
 using System.Text;
 
 using UnityEngine;
+using UnityEngine.UI;
+
+using RTFunctions.Functions;
+using RTFunctions.Functions.Data;
 
 namespace EditorManagement.Functions.Helpers
 {
@@ -33,6 +37,26 @@ namespace EditorManagement.Functions.Helpers
 
             EditorManager.inst.EditorDialogs.Add(editorPropertiesDialog);
             EditorManager.inst.EditorDialogsDictionary.Add(_name, editorPropertiesDialog);
+        }
+
+        public static void AddEditorDropdown(string name, string key, string dropdown, Sprite sprite, UnityEngine.Events.UnityAction unityAction)
+        {
+            if (!GameObject.Find($"Editor Systems/Editor GUI/sizer/main/TitleBar/{dropdown}"))
+                return;
+
+            var propWin = GameObject.Find("Editor Systems/Editor GUI/sizer/main/TitleBar/Edit/Edit Dropdown/Cut").Duplicate(GameObject.Find($"Editor Systems/Editor GUI/sizer/main/TitleBar/{dropdown}/{dropdown} Dropdown").transform, name);
+            propWin.transform.Find("Text").GetComponent<Text>().text = name;
+            propWin.transform.Find("Text").GetComponent<RectTransform>().sizeDelta = new Vector2(224f, 0f);
+            propWin.transform.Find("Text 1").GetComponent<Text>().text = key;
+
+            var propWinButton = propWin.GetComponent<Button>();
+            propWinButton.onClick.ClearAll();
+            propWinButton.onClick.AddListener(unityAction);
+
+            propWin.SetActive(true);
+
+            propWin.transform.Find("Image").GetComponent<Image>().sprite = sprite;
+
         }
     }
 }
