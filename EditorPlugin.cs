64c5ecf6dc0f5b12c072220ff8c672f1e6ea8b1d
RTMecha <mechaprower2022@gmail.com>
2023-12-18 07:23:43 +1100

    Remove todo

--- EditorPlugin.cs
+++ EditorPlugin.cs
@@ -23,13 +23,6 @@ namespace EditorManagement
     [BepInPlugin("com.mecha.editormanagement", "EditorManagement", "2.0.1")]
     public class EditorPlugin : BaseUnityPlugin
     {
-        // TO DO BEFORE RELEASE
-        // Replace "settings" with a duplicate of the other dropdowns and put the "Level Settings" as one of the tabs.
-		// Redo keyframe system because it's really annoying to deal with internal "type" and "index" stuff. Instead gotta make a wrapped list...
-		// And probably redo the keyframe creation, rendering and dragging system.
-		// Add Copy Keyframe Data button.
-		// Redo every Hovertooltip.
-
         public static EditorPlugin inst;
         public static string className = $"[<color=#F6AC1A>Editor</color><color=#2FCBD6>Management</color>] {PluginInfo.PLUGIN_VERSION}\n";
         readonly Harmony harmony = new Harmony("EditorManagement");
