3acb9150446bd692a468010830311fc09b9d39c4
RTMecha <mechaprower2022@gmail.com>
2023-12-24 16:39:07 +1100

    Added Folder Browsing

--- Functions/Editors/RTFileBrowser.cs
+++ Functions/Editors/RTFileBrowser.cs
@@ -3,9 +3,12 @@ using System.IO;
 using LSFunctions;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.EventSystems;
 using RTFunctions.Functions;
 
 using TMPro;
+
+using RTFunctions.Functions.Components;
 
 namespace EditorManagement.Functions.Editors
 {
@@ -19,13 +22,14 @@ namespace EditorManagement.Functions.Editors
         void Awake()
         {
 			inst = this;
-        }
+			title = transform.Find("Panel/Text").GetComponent<TextMeshProUGUI>();
+		}
 
 		public void UpdateBrowser(string _folder, string fileExtension, string specificName = "", Action<string> onSelectFile = null)
 		{
 			if (Directory.Exists(_folder))
 			{
-				transform.Find("Panel/Text").GetComponent<TextMeshProUGUI>().text = $"<b>File Browser</b> ({fileExtension.ToLower()})";
+				title.text = $"<b>File Browser</b> ({fileExtension.ToLower()})";
 
 				var dir = transform.Find("folder-bar").GetComponent<InputField>();
 				dir.onValueChanged.ClearAll();
@@ -39,7 +43,7 @@ namespace EditorManagement.Functions.Editors
 				{
 					_folder
 				});
-				DirectoryInfo directoryInfo = new DirectoryInfo(_folder);
+				var directoryInfo = new DirectoryInfo(_folder);
 				defaultDir = _folder;
 				string[] directories = Directory.GetDirectories(defaultDir);
 				string[] files = Directory.GetFiles(defaultDir);
@@ -68,7 +72,7 @@ namespace EditorManagement.Functions.Editors
 				for (int i = 0; i < array.Length; i++)
 				{
 					string fileName = array[i];
-					FileInfo fileInfoFolder = new FileInfo(fileName);
+					var fileInfoFolder = new FileInfo(fileName);
 					string name = fileInfoFolder.Name;
 					if (fileInfoFolder.Extension.ToLower() == fileExtension.ToLower() && (specificName == "" || specificName.ToLower() + fileExtension.ToLower() == name.ToLower()))
 					{
@@ -81,6 +85,64 @@ namespace EditorManagement.Functions.Editors
 							onSelectFile?.Invoke(fileInfoFolder.FullName);
 						});
 					}
+				}
+				folderBar.text = defaultDir;
+				return;
+			}
+			EditorManager.inst.DisplayNotification("Folder doesn't exist.", 2f, EditorManager.NotificationType.Error);
+		}
+
+		public void UpdateBrowser(string _folder, string specificName = "", Action<string> onSelectFolder = null)
+		{
+			if (Directory.Exists(_folder))
+			{
+				title.text = $"<b>File Browser</b> (Right click on a folder to use)";
+
+				var dir = transform.Find("folder-bar").GetComponent<InputField>();
+				dir.onValueChanged.ClearAll();
+				dir.onValueChanged.AddListener(delegate (string _val)
+				{
+					UpdateBrowser(_val, specificName, onSelectFolder);
+				});
+
+				LSHelpers.DeleteChildren(viewport);
+				Debug.LogFormat("Update Browser: [{0}]", new object[]
+				{
+					_folder
+				});
+				var directoryInfo = new DirectoryInfo(_folder);
+				defaultDir = _folder;
+				string[] directories = Directory.GetDirectories(defaultDir);
+				if (directoryInfo.Parent != null)
+				{
+					string backStr = directoryInfo.Parent.FullName;
+					var gameObject = backPrefab.Duplicate(viewport, backStr);
+					gameObject.GetComponent<Button>().onClick.AddListener(delegate ()
+					{
+						UpdateBrowser(backStr, specificName, onSelectFolder);
+					});
+				}
+				string[] array = directories;
+				for (int i = 0; i < array.Length; i++)
+				{
+					string folder = array[i];
+					string name = new DirectoryInfo(folder).Name;
+					var gameObject = folderPrefab.Duplicate(viewport, name);
+					gameObject.transform.Find("Text").GetComponent<Text>().text = name;
+					gameObject.GetComponent<Button>().onClick.AddListener(delegate ()
+					{
+						UpdateBrowser(folder, specificName, onSelectFolder);
+					});
+
+					var clickable = gameObject.AddComponent<Clickable>();
+					clickable.onDown = delegate (PointerEventData pointerEventData)
+					{
+						if (pointerEventData.button == PointerEventData.InputButton.Right)
+						{
+							onSelectFolder?.Invoke(folder);
+						}
+					};
+
 				}
 				folderBar.text = defaultDir;
 				return;
@@ -101,5 +163,7 @@ namespace EditorManagement.Functions.Editors
 		public InputField oggFileInput;
 
 		public string defaultDir = "";
+
+		public TextMeshProUGUI title;
 	}
 }
