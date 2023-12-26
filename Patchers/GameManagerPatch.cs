e49dbf2e445f7582b49df94ea5f1d178a92813c7
RTMecha <mechaprower2022@gmail.com>
2023-12-26 15:03:09 +1100

    Added timeline preview

---
+++ Patchers/GameManagerPatch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+
+using EditorManagement.Functions.Editors;
+
+namespace EditorManagement.Patchers
+{
+    [HarmonyPatch(typeof(GameManager))]
+    public class GameManagerPatch
+    {
+        [HarmonyPatch("UpdateTimeline")]
+        [HarmonyPostfix]
+        static void UpdateTimelinePostfix()
+        {
+            if (RTEditor.inst)
+                RTEditor.inst.UpdateTimeline();
+        }
+    }
+}
