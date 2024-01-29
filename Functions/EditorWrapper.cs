4ca18703b67b01afeb61de02eecdca638361483e
RTMecha <mechaprower2022@gmail.com>
2024-01-29 12:48:54 +1100

    Replacement for LevelFolder in editor

---
+++ Functions/EditorWrapper.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+using RTFunctions.Functions.Data;
+
+namespace EditorManagement.Functions
+{
+    public class EditorWrapper : EditorManager.MetadataWrapper
+    {
+        public EditorWrapper(GameObject gameObject, Metadata metadata, string path, Sprite sprite) : base(metadata, path, sprite)
+        {
+            GameObject = gameObject;
+        }
+
+        public GameObject GameObject { get; set; }
+
+        public void SetActive(bool active) => GameObject?.SetActive(active);
+    }
+}
