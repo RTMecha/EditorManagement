8b7f0f336343705acbc1fc4a20895359331bc8ec
RTMecha <mechaprower2022@gmail.com>
2024-02-01 14:18:54 +1100

    Prefab Panel pooling

---
+++ Functions/Components/PrefabPanel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+using RTFunctions.Functions.Data;
+
+namespace EditorManagement.Functions.Components
+{
+    public class PrefabPanel : MonoBehaviour
+    {
+        public Button Button { get; set; }
+
+        public Button DeleteButton { get; set; }
+
+        public Text Name { get; set; }
+
+        public Text TypeText { get; set; }
+
+        public Image TypeImage { get; set; }
+
+        public PrefabDialog Dialog { get; set; }
+
+        public Prefab Prefab { get; set; }
+    }
+}
