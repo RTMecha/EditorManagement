894da907bac731137ec0c868ead3227b6333dd9d
RTMecha <mechaprower2022@gmail.com>
2024-02-27 17:08:37 +1100

    Rework CheckpointEditor to be more consistent with other editor components

--- Patchers/CheckpointEditorPatch.cs
+++ Patchers/CheckpointEditorPatch.cs
@@ -12,6 +12,8 @@ using UnityEngine.UI;
 using EditorManagement.Functions.Editors;
 
 using RTFunctions.Functions;
+using UnityEngine.EventSystems;
+using EditorManagement.Functions.Helpers;
 
 namespace EditorManagement.Patchers
 {
@@ -32,11 +34,164 @@ namespace EditorManagement.Patchers
 				return false;
 			}
 
+			__instance.className = "[<color=$65B6F7>CheckpointEditor</color>] \n";
+
 			Debug.Log($"{__instance.className}" +
 				$"---------------------------------------------------------------------\n" +
 				$"---------------------------- INITIALIZED ----------------------------\n" +
 				$"---------------------------------------------------------------------\n");
 
+			return false;
+		}
+
+		[HarmonyPatch("OpenDialog")]
+		[HarmonyPrefix]
+		static bool OpenDialog(int __0)
+		{
+			int check = __0;
+
+			EditorManager.inst.ClearDialogs();
+			EditorManager.inst.ShowDialog("Checkpoint Editor");
+
+			if (Instance.right == null)
+				Instance.right = EditorManager.inst.GetDialog("Checkpoint Editor").Dialog.Find("data/right");
+
+			if (Instance.left == null)
+				Instance.left = EditorManager.inst.GetDialog("Checkpoint Editor").Dialog.Find("data/left");
+			
+			Instance.currentObj = __0;
+
+			var checkpoint = DataManager.inst.gameData.beatmapData.checkpoints[__0];
+
+			var search = Instance.right.Find("search").GetComponent<InputField>();
+			search.onValueChanged.RemoveAllListeners();
+			search.onValueChanged.AddListener(delegate (string _val)
+			{
+				Instance.RenderCheckpointList(_val, __0);
+			});
+			Instance.RenderCheckpointList(search.text, __0);
+
+			var first = Instance.left.transform.Find("edit/<<").GetComponent<Button>();
+			var prev = Instance.left.transform.Find("edit/<").GetComponent<Button>();
+			var next = Instance.left.transform.Find("edit/>").GetComponent<Button>();
+			var last = Instance.left.transform.Find("edit/>>").GetComponent<Button>();
+
+			var isFirst = __0 == 0;
+			var isLast = __0 == DataManager.inst.gameData.beatmapData.checkpoints.Count - 1;
+
+			var text = isFirst ? "S" : isLast ? "E" : check.ToString();
+
+			var delete = Instance.left.transform.Find("edit/del").GetComponent<Button>();
+
+			var name = Instance.left.Find("name").GetComponent<InputField>();
+
+			var time = Instance.left.Find("time/time").GetComponent<InputField>();
+
+			Instance.left.Find("time/<<").GetComponent<Button>().interactable = !isFirst;
+			Instance.left.Find("time/<").GetComponent<Button>().interactable = !isFirst;
+			Instance.left.Find("time/>").GetComponent<Button>().interactable = !isFirst;
+			Instance.left.Find("time/>>").GetComponent<Button>().interactable = !isFirst;
+
+			first.interactable = !isFirst;
+			prev.interactable = !isFirst;
+			delete.interactable = !isFirst;
+			time.interactable = !isFirst;
+
+			first.onClick.RemoveAllListeners();
+			prev.onClick.RemoveAllListeners();
+			delete.onClick.RemoveAllListeners();
+			time.onValueChanged.RemoveAllListeners();
+			time.text = checkpoint.time.ToString();
+
+			if (!isFirst)
+			{
+				first.onClick.AddListener(() => Instance.SetCurrentCheckpoint(0));
+
+				prev.onClick.AddListener(() => Instance.SetCurrentCheckpoint(__0 - 1));
+
+				delete.onClick.AddListener(() => Instance.DeleteCheckpoint(__0));
+
+				time.onValueChanged.AddListener(delegate (string _val)
+				{
+					if (float.TryParse(_val, out float num))
+                    {
+						checkpoint.time = num;
+						Instance.RenderCheckpoint(check);
+						GameManager.inst.UpdateTimeline();
+					}
+				});
+
+				TriggerHelper.IncreaseDecreaseButtons(time, t: Instance.left.Find("time"));
+			}
+
+			Instance.left.transform.Find("edit/|").GetComponentInChildren<Text>().text = text;
+			next.interactable = !isLast;
+			last.interactable = !isLast;
+
+			next.onClick.RemoveAllListeners();
+			last.onClick.RemoveAllListeners();
+
+			if (!isLast)
+			{
+				next.onClick.AddListener(() => Instance.SetCurrentCheckpoint(__0 + 1));
+
+				last.onClick.AddListener(() => Instance.SetCurrentCheckpoint(DataManager.inst.gameData.beatmapData.checkpoints.Count - 1));
+			}
+
+			name.onValueChanged.RemoveAllListeners();
+			name.text = checkpoint.name.ToString();
+			name.onValueChanged.AddListener(delegate (string _val)
+			{
+				checkpoint.name = _val;
+				Instance.RenderCheckpointList(search.text, __0);
+			});
+
+			var timeEventTrigger = Instance.left.Find("time").GetComponent<EventTrigger>();
+			timeEventTrigger.triggers.Clear();
+			if (!isFirst)
+				timeEventTrigger.triggers.Add(TriggerHelper.ScrollDelta(time));
+
+			var positionX = Instance.left.Find("position/x").GetComponent<InputField>();
+			var positionY = Instance.left.Find("position/y").GetComponent<InputField>();
+
+			positionX.onValueChanged.RemoveAllListeners();
+			positionX.text = checkpoint.pos.x.ToString();
+			positionX.onValueChanged.AddListener(delegate (string _val)
+			{
+				if (float.TryParse(_val, out float num))
+                {
+					checkpoint.pos.x = num;
+					Instance.RenderCheckpoint(check);
+				}
+			});
+
+			positionY.onValueChanged.RemoveAllListeners();
+			positionY.text = checkpoint.pos.y.ToString();
+			positionY.onValueChanged.AddListener(delegate (string _val)
+			{
+				if (float.TryParse(_val, out float num))
+                {
+					checkpoint.pos.y = num;
+					Instance.RenderCheckpoint(check);
+				}
+			});
+
+			TriggerHelper.IncreaseDecreaseButtons(positionX, 5f);
+			TriggerHelper.IncreaseDecreaseButtons(positionY, 5f);
+
+			TriggerHelper.AddEventTriggerParams(positionX.gameObject, TriggerHelper.ScrollDelta(positionX), TriggerHelper.ScrollDeltaVector2(positionX, positionY, 0.1f, 10f));
+			TriggerHelper.AddEventTriggerParams(positionY.gameObject, TriggerHelper.ScrollDelta(positionY), TriggerHelper.ScrollDeltaVector2(positionX, positionY, 0.1f, 10f));
+
+			Instance.RenderCheckpoints();
+
+			return false;
+		}
+
+		[HarmonyPatch("CreateNewCheckpoint", new Type[] { })]
+		[HarmonyPrefix]
+		static bool CreateNewCheckpointPrefix()
+		{
+			Instance.CreateNewCheckpoint(EditorManager.inst.CurrentAudioPos, EventManager.inst.cam.transform.position);
 			return false;
 		}
 
