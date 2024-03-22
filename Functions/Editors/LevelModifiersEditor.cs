b6fa71052dfddd0b5fb5111b0c908c8d86102560
RTMecha <mechaprower2022@gmail.com>
2024-03-22 22:59:25 +1100

    Started working on Level Modifiers Editor

---
+++ Functions/Editors/LevelModifiersEditor.cs
@@ -0,0 +1,38 @@
+﻿using RTFunctions.Functions.Data;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+
+namespace EditorManagement.Functions.Editors
+{
+    public class LevelModifiersEditor : MonoBehaviour
+    {
+        public static LevelModifiersEditor inst;
+
+        void Awake()
+        {
+            inst = this;
+        }
+
+        public void OpenDialog()
+        {
+
+        }
+
+        public IEnumerator RefreshModifiers()
+        {
+            var levelModifiers = GameData.Current.levelModifiers;
+            for (int i = 0; i < levelModifiers.Count; i++)
+            {
+                var levelModifier = levelModifiers[i];
+
+                
+            }
+
+            yield break;
+        }
+    }
+}
