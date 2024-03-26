49505f134cd37098cf8d2691227bec5091b4d8eb
RTMecha <mechaprower2022@gmail.com>
2024-03-26 13:21:36 +1100

    Version: 2.4.10

--- EditorPlugin.cs
+++ EditorPlugin.cs
@@ -24,7 +24,7 @@ using RTFunctions.Functions.Data;
 
 namespace EditorManagement
 {
-    [BepInPlugin("com.mecha.editormanagement", "EditorManagement", "2.4.9")]
+    [BepInPlugin("com.mecha.editormanagement", "EditorManagement", "2.4.10")]
     public class EditorPlugin : BaseUnityPlugin
     {
         public static EditorPlugin inst;
