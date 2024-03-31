fcf0d4ba42b940bd150de94af409896229bef9b1
RTMecha <mechaprower2022@gmail.com>
2024-03-31 23:43:56 +1100

    Multi Object Editor rendering

---
+++ Functions/Components/AssignToTheme.cs
@@ -0,0 +1,72 @@
+﻿using RTFunctions.Functions.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace EditorManagement.Functions.Components
+{
+    public class AssignToTheme : MonoBehaviour
+    {
+        public Graphic Graphic { get; set; }
+        public int Index { get; set; }
+        public Type ThemeType { get; set; } = Type.Objects;
+        public enum Type
+        {
+            GUI,
+            Background,
+            Player,
+            PlayerTail,
+            Objects,
+            BackgroundObjects,
+            Effects
+        }
+
+        void Update()
+        {
+            if (!gameObject.activeInHierarchy || !Graphic.isActiveAndEnabled)
+                return;
+
+            switch (ThemeType)
+            {
+                case Type.GUI:
+                    {
+                        Graphic.color = RTHelpers.BeatmapTheme.guiColor;
+                        break;
+                    }
+                case Type.PlayerTail:
+                    {
+                        Graphic.color = RTHelpers.BeatmapTheme.guiAccentColor;
+                        break;
+                    }
+                case Type.Background:
+                    {
+                        Graphic.color = RTHelpers.BeatmapTheme.backgroundColor;
+                        break;
+                    }
+                case Type.Player:
+                    {
+                        Graphic.color = RTHelpers.BeatmapTheme.GetPlayerColor(Index);
+                        break;
+                    }
+                case Type.Objects:
+                    {
+                        Graphic.color = RTHelpers.BeatmapTheme.GetObjColor(Index);
+                        break;
+                    }
+                case Type.BackgroundObjects:
+                    {
+                        Graphic.color = RTHelpers.BeatmapTheme.GetBGColor(Index);
+                        break;
+                    }
+                case Type.Effects:
+                    {
+                        Graphic.color = RTHelpers.BeatmapTheme.GetFXColor(Index);
+                        break;
+                    }
+            }
+        }
+    }
+}
---
+++ Functions/Components/UpdateMultiObjectInfo.cs
@@ -0,0 +1,27 @@
+﻿using EditorManagement.Functions.Editors;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace EditorManagement.Functions.Components
+{
+    public class UpdateMultiObjectInfo : MonoBehaviour
+    {
+        public Text Text { get; set; }
+
+        public string DefaultText => "You are currently editing multiple objects.\n\nObject Count: {0}/{3}\nPrefab Object Count: {1}/{4}\nTotal: {2}";
+
+        void Update()
+        {
+            if (!Text || !Text.isActiveAndEnabled)
+                return;
+
+            Text.text = string.Format(DefaultText,
+                ObjectEditor.inst.SelectedObjects.Count,
+                ObjectEditor.inst.SelectedPrefabObjects.Count,
+                ObjectEditor.inst.SelectedObjects.Count + ObjectEditor.inst.SelectedPrefabObjects.Count,
+                DataManager.inst.gameData.beatmapObjects.Count,
+                DataManager.inst.gameData.prefabObjects.Count);
+        }
+    }
+}
