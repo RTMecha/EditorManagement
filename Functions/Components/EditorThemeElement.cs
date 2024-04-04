7c7017212d9d47cf769a9bd5e726fb84939f4ba0
RTMecha <mechaprower2022@gmail.com>
2024-04-04 21:16:26 +1100

    Fallback component in case the current theme changing method is too slow.

---
+++ Functions/Components/EditorThemeElement.cs
@@ -0,0 +1,38 @@
+﻿using EditorManagement.Functions.Editors;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace EditorManagement.Functions.Components
+{
+    /// <summary>
+    /// Fallback component in case the current method is too slow.
+    /// </summary>
+    public class EditorThemeElement : MonoBehaviour
+    {
+        bool init = false;
+        int lastTheme = 0;
+
+        public EditorThemeManager.Element Element { get; set; }
+
+        public void Init(EditorThemeManager.Element element)
+        {
+            Element = element;
+            Element.ApplyTheme(EditorThemeManager.CurrentTheme);
+            lastTheme = EditorThemeManager.currentTheme;
+            init = true;
+        }
+
+        void FixedUpdate()
+        {
+            if (init && lastTheme != EditorThemeManager.currentTheme)
+            {
+                Element.ApplyTheme(EditorThemeManager.CurrentTheme);
+                lastTheme = EditorThemeManager.currentTheme;
+            }
+        }
+    }
+}
