49772839c6023069c48ac0cc8dcfa11793709126
RTMecha <mechaprower2022@gmail.com>
2024-04-05 23:08:04 +1100

    For UI elements that need to contrast to their background

---
+++ Functions/Components/ContrastColors.cs
@@ -0,0 +1,29 @@
+﻿using LSFunctions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace EditorManagement.Functions.Components
+{
+    public class ContrastColors : MonoBehaviour
+    {
+        public Graphic Graphic { get; set; }
+        public Graphic BaseGraphic { get; set; }
+
+        public void Init(Graphic graphic, Graphic baseGraphic)
+        {
+            Graphic = graphic;
+            BaseGraphic = baseGraphic;
+        }
+
+        void Update()
+        {
+            if (Graphic && BaseGraphic)
+                Graphic.color = LSColors.ContrastColor(BaseGraphic.color);
+        }
+    }
+}
