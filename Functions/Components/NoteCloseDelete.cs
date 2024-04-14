4abaa8e1d13f14ca6083023ab00894f2fe47476c
RTMecha <mechaprower2022@gmail.com>
2024-04-14 22:15:57 +1000

    Change close /delete button depending on situation

---
+++ Functions/Components/NoteCloseDelete.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace EditorManagement.Functions.Components
+{
+    public class NoteCloseDelete : MonoBehaviour
+    {
+        public GameObject delete;
+        public GameObject close;
+
+        public void Init(GameObject delete, GameObject close)
+        {
+            this.delete = delete;
+            this.close = close;
+        }
+
+        void Update()
+        {
+            delete?.SetActive(EditorManager.inst.editorState == EditorManager.EditorState.Intro && ProjectPlannerManager.inst.CurrentTab == 5);
+            close?.SetActive(EditorManager.inst.editorState == EditorManager.EditorState.Main || ProjectPlannerManager.inst.CurrentTab != 5);
+        }
+    }
+}
