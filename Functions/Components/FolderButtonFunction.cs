fcca8e32bd2a5ff28dd0c116534331ccfb007f65
RTMecha <mechaprower2022@gmail.com>
2024-04-16 12:20:34 +1000

    Fix for on drag bug

---
+++ Functions/Components/FolderButtonFunction.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace EditorManagement.Functions.Components
+{
+    public class FolderButtonFunction : MonoBehaviour, IPointerClickHandler
+    {
+        public Action<PointerEventData> onClick;
+
+        public void OnPointerClick(PointerEventData eventData) => onClick?.Invoke(eventData);
+    }
+}
