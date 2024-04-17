4362eca97671186debc2e9eff84dd961dbe4d338
RTMecha <mechaprower2022@gmail.com>
2024-04-18 05:59:29 +1000

    Version: 2.5.8

--- EditorPlugin.cs
+++ EditorPlugin.cs
@@ -18,7 +18,7 @@ using UnityEngine.UI;
 
 namespace EditorManagement
 {
-    [BepInPlugin("com.mecha.editormanagement", "EditorManagement", "2.5.7")]
+    [BepInPlugin("com.mecha.editormanagement", "EditorManagement", "2.5.8")]
     public class EditorPlugin : BaseUnityPlugin
     {
         public static EditorPlugin inst;
